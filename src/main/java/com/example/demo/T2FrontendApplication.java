a0ac872b9203435412527ef5d523d73a6eaee611
Jireh Flores <Jirekcz03ft@gmail.com>
2024-09-24 11:29:37 -0500

    Carga inicial del proyecto

---
+++ src/main/java/com/example/demo/T2FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2FrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2FrontendApplication.class, args);
+	}
+
+}
