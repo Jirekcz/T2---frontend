dbd777a5f5eb75de17b7a59cf51126cd0c087aca
Jireh Flores <Jirekcz03ft@gmail.com>
2024-09-24 13:19:16 -0500

    Frontend casi terminado

--- src/main/java/com/example/demo/controller/InicioController.java
+++ src/main/java/com/example/demo/controller/InicioController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.web.client.RestTemplate;
+import com.example.demo.dto.InicioRequestDTO;
+import com.example.demo.dto.InicioResponseDTO;
 import com.example.demo.viewmodel.InicioModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/main")
 public class InicioController {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/inicio")
     public String inicio(Model model) {
         InicioModel inicioModel = new InicioModel("00", "", "");
@@ -21,9 +28,35 @@ public class InicioController {
 
     @PostMapping("/buscar")
     public String buscar(@RequestParam("placa") String placa, Model model) {
-        // Validar que los campos no esten vacios
-        InicioModel inicioModel = new InicioModel("01", "ERROR: Debe completar los campos correctamente", "");
-        model.addAttribute("inicioModel", inicioModel);
-        return "inicio";
+        // Validamos los campos de entrada
+        if (placa == null || placa.trim().length() == 0) {
+            InicioModel inicioModel = new InicioModel("01", "ERROR: Completar los campos", "");
+            model.addAttribute("inicioModel", inicioModel);
+            return "inicio";
+        }
+
+        try {
+            // Invocamos al API
+            String endpoint = "";
+            InicioRequestDTO inicioRequestDTO = new InicioRequestDTO(placa);
+            InicioResponseDTO inicioResponseDTO = restTemplate.postForObject(endpoint, inicioRequestDTO, InicioResponseDTO.class);
+
+            // Validar respuesta
+            if (inicioResponseDTO.codigo().equals("00")) {
+                InicioModel inicioModel = new InicioModel("00", "", inicioResponseDTO.marca());
+                model.addAttribute("inicioModel", inicioModel);
+                return "principal";
+            } else {
+                InicioModel inicioModel = new InicioModel("02", "ERROR: Busqueda fallida", "");
+                model.addAttribute("inicioModel", inicioModel);
+                return "inicio";
+            }
+
+        } catch (Exception e) {
+            InicioModel inicioModel = new InicioModel("01", "", "");
+            model.addAttribute("inicioModel", inicioModel);
+            System.out.println(e.getMessage());
+            return "inicio";
+        }
     }
 }
---
+++ src/main/java/com/example/demo/dto/InicioResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.demo.dto;
+
+public record InicioResponseDTO(String codigo, String mensaje, String marca, String modelo, String nroAsientos, String precio, String color) {
+}
